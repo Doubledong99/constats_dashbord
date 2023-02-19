939814acce8d411935bb24369247b2198cefeec2
Dongdong LI <65075564+Doubledong99@users.noreply.github.com>
2023-02-19 16:06:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,40 +9,4 @@ st.set_page_config(
   layout="wide"                 
 )
 
-df= pd.read_excel(
-      io='Data_constats.xlsx',
-      engine='openpyxl',
-      sheet_name='Constats_compilation',
-      skiprows=3,
-      usecols='B:R',
-      nrows=1000,
-    )
-
-
-# SIDEBAR
-st.sidebar.header("Please Filter Here:")
-
-Etat= st.sidebar.multiselect(
-  "Select the state:",
-  options=df["Etat constat"].unique(),
-  default=df["Etat constat"].unique()
-)
-
-Thème= st.sidebar.multiselect(
-  "Select the Thème:",
-  options=df["Thème"].unique(),
-  default=df["Thème"].unique()
-)
-
-Lot= st.sidebar.multiselect(
-  "Select the Lot:",
-  options=df["Lot"].unique(),
-  default=df["Lot"].unique()
-)
-
-df_selection=df.query(
-  "Etat Constat== @Etat & Thème== @Thème & Lot == @Lot"
-)
-
-st.dataframe(df_selection)
 
